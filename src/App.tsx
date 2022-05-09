4880416fd93415a9439d7654c5e56332a0423aae
mohamadkhazaee <wallex@WALLEXs-MacBook-Air-2.local>
2022-05-09 16:24:05 +0430

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useMemo, useState } from "react";
+import Cookies from "js-cookie";
+
+export const METAMASK_INFO_KEYS = {
+  ADDRESS: "METAMASK_ACCOUNT_ADDRESS",
+  SIGNITURE: "METAMASK_ACCOUNT_SIGNITURE",
+};
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const metamaskAddress = useMemo(() => {
+    if (!loading) {
+      return Cookies.get(METAMASK_INFO_KEYS.ADDRESS);
+    }
+  }, [loading]);
+
+  const handleClick = async () => {
+    setLoading(true);
+    if (metamaskAddress) {
+      Cookies.remove(METAMASK_INFO_KEYS.ADDRESS);
+      Cookies.remove(METAMASK_INFO_KEYS.SIGNITURE);
+      alert("logged out!");
+      return;
+    }
+    if (typeof window.ethereum !== "undefined") {
+      setLoading(true);
+      await window.ethereum.enable();
+      const message = "Hello from Ethereum Stack Exchange!";
+      const accounts = await window.ethereum.request({
+        method: "eth_accounts",
+      });
+      const signature = await window.ethereum.request({
+        method: "personal_sign",
+        params: [message, accounts[0]],
+      });
+      Cookies.set(METAMASK_INFO_KEYS.SIGNITURE, accounts[0]);
+      Cookies.set(METAMASK_INFO_KEYS.ADDRESS, signature);
+      setLoading(false);
+    } else {
+      alert("No Metamask Wallet Detected!");
+      setLoading(false);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleClick}>
+        {metamaskAddress ? "Connected!" : "Not Connected!"}
+      </button>
+      {metamaskAddress && (
+        <div className="info-container">
+          <h5>Wallet Address:</h5>
+          <p>{metamaskAddress}</p>
+        </div>
+      )}
     </div>
   );
 }
